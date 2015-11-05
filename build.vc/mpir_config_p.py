ef16dd187a3723ff0631e9ecd359f25b461ded38
SeVlaT <sevlat@mail.ru>
2015-11-05 19:01:50 +0300

    add_proj_to_sln() moved to _msvc_solution.py module

--- build.vc/mpir_config_p.py
+++ build.vc/mpir_config_p.py
@@ -20,6 +20,7 @@ from time import sleep
 
 from _msvc_filters import gen_filter
 from _msvc_project import gen_vcxproj, gen_project_props
+from _msvc_solution import add_proj_to_sln
 
 
 g_studio_version='12' # Default value
@@ -315,90 +316,7 @@ def gen_have_list(c, sym_dir, out_dir):
 
 
 
-# add a project file to the solution
-
-folder_guid = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}"
-vcxproject_guid = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}"
-
-s_guid = r'\s*(\{\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\})\s*'
-s_name = r'\s*\"([a-zA-Z][-.\\_a-zA-Z0-9]*\s*)\"\s*'
-re_guid = compile(r'\s*\"\s*' + s_guid + r'\"\s*')
-re_proj = compile(r'Project\s*\(\s*\"' + s_guid + r'\"\)\s*=\s*'
-                  + s_name + r'\s*,\s*' + s_name + r'\s*,\s*\"' + s_guid + r'\"')
-re_fmap = compile(r'\s*' + s_guid + r'\s*=\s*' + s_guid)
-
-def read_solution_file(soln_name):
-  fd, pd, p2f = {}, {}, {}
-  solution_path = join(solution_dir, soln_name)
-  if exists(solution_path):
-    lines = open(solution_path).readlines()
-    for i, ln in enumerate(lines):
-      m = re_proj.search(ln)
-      if m:
-        if m.group(1) == folder_guid and m.group(2) == m.group(3):
-          fd[m.group(2)] = m.group(4)
-        elif m.group(3).endswith('.vcxproj') or m.group(3).endswith('.pyproj'):
-          pd[m.group(2)] = (m.group(1), m.group(3), m.group(4))
-      m = re_fmap.search(ln)
-      if m:
-        p2f[m.group(1)] = m.group(2)
-  return fd, pd, p2f
-
-sol_1 = '''Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio {0}
-VisualStudioVersion = {1}
-MinimumVisualStudioVersion = 10.0.40219.1
-'''.format(g_sln_studio_version_short, g_sln_studio_version_long)
-
-sol_2 = '''Project("{}") = "{}", "{}", "{}"
-EndProject
-'''
-
-sol_3 = '''Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|Win32 = Debug|Win32
-		Debug|x64 = Debug|x64
-		Release|Win32 = Release|Win32
-		Release|x64 = Release|x64
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(NestedProjects) = preSolution
-'''
-
-sol_4 = '''		{} = {}
-'''
-
-sol_5 = r'''	EndGlobalSection
-EndGlobal
-'''
-
-def write_solution_file(file_name, fd, pd, p2f):
-  with open(join(solution_dir, file_name), 'w') as outf:
-    outf.write(sol_1)
-    for f, g in fd.items():
-      outf.write(sol_2.format(folder_guid, f, f, g))
-    for f, (g1, pn, g2) in pd.items():
-      outf.write(sol_2.format(g1, f, pn, g2))
-    outf.write(sol_3)
-    for f, g in p2f.items():
-      outf.write(sol_4.format(f, g))
-    outf.write(sol_5)
-
-def add_proj_to_sln(soln_name, soln_folder, proj_name, file_name, guid):
-  fd, pd, p2f = read_solution_file(soln_name)
-  if soln_folder:
-    if soln_folder in fd:
-      f_guid = fd[soln_folder]
-    else:
-      f_guid = '{' + str(uuid4()).upper() + '}'
-      fd[soln_folder] = f_guid
-  pd[proj_name] = (vcxproject_guid, file_name, guid)
-  if soln_folder:
-    p2f[guid] = f_guid
-
-  write_solution_file(soln_name, fd, pd, p2f)
+################################################################################
 # compile list of C files
 t = find_src(c_directories)
 c_hdr_list = t[0]
